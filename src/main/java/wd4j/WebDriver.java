8f28363876d15452da59a73503773843b0e44de7
Miguel Angelo Fernández Iglesias <82086544+mfernandezig@users.noreply.github.com>
2025-03-21 13:25:15 +0100

    Draft WebDriver.java

---
+++ src/main/java/wd4j/WebDriver.java
@@ -0,0 +1,19 @@
+package wd4j;
+
+/**
+ * This class is the entry point for the low-level WebDriver API. Aggregates all WebDriver Modules at one place.
+ * @see wd4j.impl.manager.WDBrowserManager
+ * @see wd4j.impl.manager.WDSessionManager
+ * @see wd4j.impl.manager.WDBrowsingContextManager
+ * @see wd4j.impl.manager.WDScriptManager
+ * @see wd4j.impl.manager.WDInputManager
+ * @see wd4j.impl.manager.WDStorageManager
+ * @see wd4j.impl.manager.WDNetworkManager
+ * @see wd4j.impl.manager.WDLogManager
+ * @see wd4j.impl.manager.WDWebExtensionManager
+ *
+ * @link https://www.w3.org/TR/webdriver-bidi/#modules
+ * @link https://de.wikipedia.org/wiki/Fassade_(Entwurfsmuster)
+ */
+public class WebDriver {
+}
