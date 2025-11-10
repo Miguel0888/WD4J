9705d0a7495219ddb5453376d161362a06775c9e
Miguel Angelo Fernández Iglesias <angelo.iglesias@gmx.de>
2025-11-10 15:31:08 +0100

    Revert "Fix Java string literals in Main constants"
    This reverts commit 7b3236facdceb23fd4a563970773004e2da53b11.

--- app/src/main/java/de/bund/zrb/Main.java
+++ app/src/main/java/de/bund/zrb/Main.java
@@ -4,16 +4,16 @@ import static de.bund.zrb.settings.SettingsBootstrap.ensureUserSettingsPresent;
 
 public class Main {
 
-    public static final String RECORD_FLAG = "-d";
-    public static final String ALT_RECORD_FLAG = "--debug";
-    public static final String DEBUG_ON_FAIL = "--debugOnFail";
-    public static final String DEBUG_PLAYERS = "--debugPlayers";
+    public static final String RECORD_FLAG = '-d';
+    public static final String ALT_RECORD_FLAG = '--debug';
+    public static final String DEBUG_ON_FAIL = '--debugOnFail';
+    public static final String DEBUG_PLAYERS = '--debugPlayers';
 
     public static void main(String[] args) {
         // Ensure default user settings are initialized once if missing
         ensureUserSettingsPresent();
 
-        // Call existing startup logic (unchanged)
+        // TODO: restore the original startup logic here if different
         de.bund.zrb.service.SettingsService.getInstance().initAdapter();
         de.bund.zrb.ui.MainFrame.main(args);
     }
